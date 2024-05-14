cedff16baad90c7c7c62b49069fdde867259fc86
jDiOt <159460193+Im-jDiOt@users.noreply.github.com>
2024-05-14 21:04:06 +0900

    Update snake.py
    food event

--- python_small_projects/snake.py
+++ python_small_projects/snake.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+from collections import deque
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
@@ -7,20 +9,27 @@ movement = 50
 key_pressed = False
 condition = 0
 running = True
+point = 0
+queue = deque(maxlen=5)
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-
-pygame.init()
-pygame.display.set_caption("Simple PyGame Example")
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+food = (255,0,0)
 
 pos_x = 200
 pos_y = 200
+food_x = random.randint(0,9)*50
+food_y = random.randint(0,9)*50
+
+pygame.init()
+pygame.display.set_caption("snake.py")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
 while running:
     clock.tick(5)
+
+    # movement
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -43,18 +52,32 @@ while running:
     if condition == 1:
         pos_x -= movement
         key_pressed = True
-    elif condition ==2:
+    elif condition == 2:
         pos_x += movement
         key_pressed = True
-    elif condition ==3:
+    elif condition == 3:
         pos_y -= movement
         key_pressed = True
-    elif condition ==4:
+    elif condition == 4:
         pos_y += movement
         key_pressed = True
 
+    queue.append((pos_x, pos_y))
+    print(queue)
+
+    # food event
+    if (pos_x==food_x) and (pos_y==food_y):
+        point += 1
+
+        food_x = random.randint(0,9)*50
+        food_y = random.randint(0,9)*50
+   
+    # screen draw
     screen.fill(black)
-    pygame.draw.rect(screen, white, (pos_x, pos_y, 50, 50))
+
+    for i in range(point+1):
+        pygame.draw.rect(screen, white, (queue[-1-i][0], queue[-1- i][1], 50, 50))
+    pygame.draw.rect(screen, food, (food_x, food_y, 50, 50))
 
     for i in range(10):     
         pygame.draw.line(screen, white, (50*i, 0), (50*i, SCREEN_HEIGHT), 1)
